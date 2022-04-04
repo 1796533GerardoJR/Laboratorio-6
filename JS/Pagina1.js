d802f80143ca09296bd75cd3e73f0fed60520f7a
1796533GerardoJR <gerardo.hdzgarcia@uanl.edu.mx>
2022-04-03 19:29:35 -0500

    Cambios del laboratorio 62

--- JS/Pagina1.js
+++ JS/Pagina1.js
@@ -1,5 +1,7 @@
 function activacion1(){
     $("ul.taps li #HPER1").addClass("active");
+    $(".secciones article").hide();
+    $(".secciones #tap1").show()
     $("#HPER2").removeClass("active");
     $("#HPER3").removeClass("active");
     $("#HPER4").removeClass("active");
@@ -7,6 +9,8 @@ function activacion1(){
 
 function activacion2(){
     $("ul.taps li #HPER2").addClass("active");
+    $(".secciones article").hide();
+    $(".secciones #tap2").show()
     $("#HPER1").removeClass("active");
     $("#HPER3").removeClass("active");
     $("#HPER4").removeClass("active");
@@ -14,6 +18,8 @@ function activacion2(){
 
 function activacion3(){
     $("ul.taps li #HPER3").addClass("active");
+    $(".secciones article").hide();
+    $(".secciones #tap3").show()
     $("#HPER1").removeClass("active");
     $("#HPER2").removeClass("active");
     $("#HPER4").removeClass("active");
@@ -21,6 +27,8 @@ function activacion3(){
 
 function activacion4(){
     $("ul.taps li #HPER4").addClass("active");
+    $(".secciones article").hide();
+    $(".secciones #tap4").show()
     $("#HPER1").removeClass("active");
     $("#HPER2").removeClass("active");
     $("#HPER3").removeClass("active");
